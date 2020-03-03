982ad1034d721753d81b7226f1e65259b25c1ccd
Grace <fhb190776@fh-vie.ac.at>
2020-03-03 10:24:38 +0100

    Auto stash before checking out "HEAD"

---
+++ PROG1/src/einheit1/BedingteAnweisung.java
@@ -0,0 +1,10 @@
+package einheit1;
+
+public class BedingteAnweisung {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ PROG1/src/einheit1/Login.java
@@ -0,0 +1,17 @@
+package einheit1;
+
+public class Login {
+
+	public static void main(String[] args) {
+		String user ="dummy";
+		String pwd ="pwd";
+		
+		if(user.equals("dummy") && pwd.equals("1234")) {
+			System.out.println("Login erfolgreich");
+		}else {
+			System.out.println("Nicht erfolgreich");
+		}
+
+	}
+
+}
